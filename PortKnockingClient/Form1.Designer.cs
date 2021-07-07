4c65a106fa117a94f57be41a337c8139a7c04303
Marek Urbański <marek@www2.pl>
2021-07-07 15:19:54 +0200

    Window Form

--- PortKnockingClient/Form1.Designer.cs
+++ PortKnockingClient/Form1.Designer.cs
@@ -29,7 +29,7 @@ namespace PortKnockingClient
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -40,36 +40,51 @@ namespace PortKnockingClient
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newConfigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
-            this.tabControl1.SuspendLayout();
+            this.commandsText = new System.Windows.Forms.TextBox();
+            this.LogsTab = new System.Windows.Forms.TabPage();
+            this.textBoxLogs = new System.Windows.Forms.TextBox();
+            this.textBoxHelp = new System.Windows.Forms.TextBox();
+            this.sendCommandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.LogsTab.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabControl
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 30);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(430, 213);
-            this.tabControl1.TabIndex = 0;
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.LogsTab);
+            this.tabControl.Location = new System.Drawing.Point(12, 30);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(431, 252);
+            this.tabControl.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.commandsText);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(422, 187);
+            this.tabPage1.Size = new System.Drawing.Size(423, 226);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Config:";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.textBoxHelp);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(422, 187);
+            this.tabPage2.Size = new System.Drawing.Size(423, 226);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Manual/Help";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -78,10 +93,11 @@ namespace PortKnockingClient
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
+            this.sendCommandsToolStripMenuItem,
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(458, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -136,19 +152,83 @@ namespace PortKnockingClient
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
             // 
+            // commandsText
+            // 
+            this.commandsText.Location = new System.Drawing.Point(3, 3);
+            this.commandsText.Multiline = true;
+            this.commandsText.Name = "commandsText";
+            this.commandsText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.commandsText.Size = new System.Drawing.Size(420, 220);
+            this.commandsText.TabIndex = 0;
+            this.commandsText.WordWrap = false;
+            // 
+            // LogsTab
+            // 
+            this.LogsTab.Controls.Add(this.textBoxLogs);
+            this.LogsTab.Location = new System.Drawing.Point(4, 22);
+            this.LogsTab.Name = "LogsTab";
+            this.LogsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.LogsTab.Size = new System.Drawing.Size(423, 226);
+            this.LogsTab.TabIndex = 2;
+            this.LogsTab.Text = "Logs";
+            this.LogsTab.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLogs
+            // 
+            this.textBoxLogs.Location = new System.Drawing.Point(1, 3);
+            this.textBoxLogs.Multiline = true;
+            this.textBoxLogs.Name = "textBoxLogs";
+            this.textBoxLogs.ReadOnly = true;
+            this.textBoxLogs.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxLogs.Size = new System.Drawing.Size(420, 220);
+            this.textBoxLogs.TabIndex = 1;
+            this.textBoxLogs.WordWrap = false;
+            // 
+            // textBoxHelp
+            // 
+            this.textBoxHelp.Location = new System.Drawing.Point(1, 3);
+            this.textBoxHelp.Multiline = true;
+            this.textBoxHelp.Name = "textBoxHelp";
+            this.textBoxHelp.ReadOnly = true;
+            this.textBoxHelp.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxHelp.Size = new System.Drawing.Size(420, 220);
+            this.textBoxHelp.TabIndex = 2;
+            this.textBoxHelp.WordWrap = false;
+            // 
+            // sendCommandsToolStripMenuItem
+            // 
+            this.sendCommandsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sendToolStripMenuItem});
+            this.sendCommandsToolStripMenuItem.Name = "sendCommandsToolStripMenuItem";
+            this.sendCommandsToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.sendCommandsToolStripMenuItem.Text = "Commands";
+            // 
+            // sendToolStripMenuItem
+            // 
+            this.sendToolStripMenuItem.Name = "sendToolStripMenuItem";
+            this.sendToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.sendToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sendToolStripMenuItem.Text = "Send";
+            // 
             // ClientKnockerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(458, 306);
+            this.Controls.Add(this.tabControl);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "ClientKnockerForm";
             this.Text = "Port Knocker Client";
-            this.tabControl1.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.LogsTab.ResumeLayout(false);
+            this.LogsTab.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,7 +236,7 @@ namespace PortKnockingClient
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -167,6 +247,12 @@ namespace PortKnockingClient
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox commandsText;
+        private System.Windows.Forms.TabPage LogsTab;
+        private System.Windows.Forms.TextBox textBoxHelp;
+        private System.Windows.Forms.TextBox textBoxLogs;
+        private System.Windows.Forms.ToolStripMenuItem sendCommandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sendToolStripMenuItem;
     }
 }
 
